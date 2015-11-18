afa8373d212a141936b7fffbb16fceaa128b9e2b
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-18 10:10:59 -0500

    solution

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
+
+  def self.all
+    DOGS
+  end
 
 end
+
