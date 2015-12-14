fca22fbf61b06ba4bbb35fe1475ef499181d5361
onyoo <roberto9m9@gmail.com>
2015-12-14 15:37:19 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-  	erb :index
-    new_dog = Dog.new("Spot","dalmation",1000)
-    "<p>My dog's breed is #{new_dog.breed}</p>"
+    erb :index
+    @new_dog = Dog.new("Spot","dalmation",1000)
+    "<p>My dog's breed is #{@new_dog.breed}</p>"
   end
 end
