0ee986ddd563f931fff2d7018d0ac41a1aa7c009
macgeek30 <mike25ko@gmail.com>
2016-01-16 03:22:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < Sinatra::Base
 
+  attr_accessor :dog
+
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
   get "/" do
-  	erb :index
+  	@dog = Dog.new("rudolph", "mastiff", 2)
+    erb :index
   end
+
+
+
 end
