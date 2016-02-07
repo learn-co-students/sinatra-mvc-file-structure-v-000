417f051ebcaf7f2d13710bd030aae889b9c20180
Chris Bruen <bruen.chris@gmail.com>
2016-02-07 14:40:11 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
+  attr_accessor :dog
 
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
-
+	
   get "/" do
+    @dog = Dog.new("Spke", "dalmation", 4)
   	erb :index
+  	"<p>My dog's breed is #{@dog.breed}</p>"
   end
 end
