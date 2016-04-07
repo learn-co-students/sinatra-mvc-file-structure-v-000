ab057420a4ce622d1f43c4f2f2fb3214ef3aac97
Ann Lee <aplee29@users.noreply.github.com>
2016-04-06 22:26:18 -0400

    complete Sinatra MVC application

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
 
+  DOGS = []
+
+  def initialize(name, breed, age)
+    @name, @breed, @age = name, breed, age
+    DOGS << self
+  end
+
+  def self.all
+    DOGS
+  end
 end
