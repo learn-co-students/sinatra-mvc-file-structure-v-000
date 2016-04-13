2d936dd642413a6932151d42ba88c4f5973ff171
mendelB <mendelblesofsky@gmail.com>
2016-04-13 17:46:26 -0400

    update solution branch to reflect the changes to master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < Sinatra::Base
 
-configure do
-  set :views, "app/views"
-  set :public_dir, "public"
-end
+  configure do
+    set :views, "app/views"
+    set :public_dir, "public"
+  end
 
-get '/' do
-    @dog = Dog.new("ralph", "dalmation", 6)
-
-  erb :index
-end
-
+  get '/' do
+    erb :index
+  end
 end
