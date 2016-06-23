58df27840c58a7e4d88c767280faf0c3bfed8901
kaileeagray <kaileeagray@gmail.com>
2016-06-23 13:35:35 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+# connect up all the files in our application to the appropriate gems and to each other.
+# This environment.rb file loads Bundler and thus all the gems in our Gemfile,
+# as well as the app directory`.
+
 ENV['SINATRA_ENV'] ||= "development"
 ENV['RACK_ENV'] ||= "development"
 
