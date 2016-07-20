8ff30780da271d80c46278d2e988dea84eedb3dc
Jason Casares <jasonc@csdrafting.com>
2016-07-20 15:04:44 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  def self.all
+  
+  end
+
+
+end
