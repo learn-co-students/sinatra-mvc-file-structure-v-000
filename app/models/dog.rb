cae51f0d4500a004b32dd33dcf297738a50c820e
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-29 03:46:14 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,13 +3,10 @@ class Dog
   @@all = []
 
 
-  def initialization(name, breed, age)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-  end
-
-  def save
     @@all << self
   end
 
