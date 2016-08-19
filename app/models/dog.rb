a9999dfff0d40c007a4769025edd466e8dbe4166
ThisIsTyDell <milleb94@gmail.com>
2016-08-19 15:54:15 -0500

    updated Dog Class to be more representitve of dogs

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,16 @@
 class Dog
   attr_accessor :name, :breed, :age
 
-  @@all = []
+  DOGS = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@all << self
+    DOGS << self
   end
 
   def self.all
-    @@all
+    DOGS
   end
 end
