122b895ae20cf788cc71c5dafc738de745a8b019
Levio Marotti III <lm3@protonmail.com>
2017-03-09 14:11:14 -0600

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,16 @@
 class Dog
   attr_accessor :name, :breed, :age
 
-  @@dogs = []
+  DOGS = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@dogs << self
+    DOGS << self
   end
 
   def self.all
-    @@dogs 
+    DOGS
   end
 end
