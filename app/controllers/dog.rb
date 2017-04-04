097293ef45d106eb8f1215a48d719355ce3c2616
Seth <itz.saga@gmail.com>
2017-04-03 21:53:31 -0500

    Done.

---
+++ app/controllers/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  attr_accessor :name, :age
+  attr_reader :breed
+
+  @@all = []
+
+  def initialize(name = nil, breed = nil, age = nil)
+    @name = name
+    @age = age
+    @breed = breed
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
