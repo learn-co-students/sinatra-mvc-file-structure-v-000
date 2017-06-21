2acff30fc4897c0893545c8a7bd423092950b887
jordan surgick <jordansurgick@aol.com>
2017-06-21 14:02:03 +0000

    Done.

---
+++ app/models/Dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self unless @@all.include?(self.name)
+  end
+
+  def self.all
+    @@all
+  end
+end
