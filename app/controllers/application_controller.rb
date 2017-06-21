f5c8bbd42f6fe4b4ead7ca937f955e7ae3f421f8
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-06-21 21:34:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
-class Dog
-  attr_accessor :name, :breed, :age
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def initialize(name = nil, breed = nil, age = nil)
-    @name = name
-    @breed = breed
-    @age = age
-    @@all << self
-  end
-end
 class ApplicationController < Sinatra::Base
 
   configure do
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def initialize(name = nil, breed = nil, age = nil)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
 end
