8420b672babc409420d059077c0312c3eeb80f21
Andrew <github email address>
2017-06-26 13:43:21 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,26 @@
-class Model
+class Dog
+    @@all = []
+
+    attr_accessor :name, :breed, :age
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    def name
+        @name
+
+    end
+    def breed
+        @breed
+    end
+    def age
+        @age
+    end
 
 end
