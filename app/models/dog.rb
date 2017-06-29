f6c76fb86b50823819388cd262a9f8261c878d3a
Scott Wakeman <swakeman@ca.rr.com>
2017-06-29 22:59:13 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,17 +1,15 @@
 class Dog
-attr_accessor :name, :breed, :age
+  attr_accessor :name, :breed, :age
+  @@all = []
 
-@@all = []
+  def initialize(name = nil, breed = nil, age = nil)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
 
-def initialize(name = nil, breed = nil, age = nil)
-  @name = name
-  @breed = breed
-  @age = age
-  @@all << self
-end
-
-def self.all
-  @@all
-end
-
+  def self.all
+    @@all
+  end
 end
