4cb216e0442e2ee96ff2274ad24f6e398cce4a3f
Scott Wakeman <swakeman@ca.rr.com>
2017-06-29 22:55:06 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,29 +1,17 @@
-#create a dog class
 class Dog
-  #You should be able to read and write to these attributes
-  attr_accessor :name, :breed, :age
-#class method all to return an array of those instances.
-  @@all = []
+attr_accessor :name, :breed, :age
 
-#This class should have name, breed, and age attributes which can be set on initialization
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-    @@all << self
-  end
+@@all = []
 
-#class method all to return an array of those instances.
-  def self.all
-    @@all
-  end
+def initialize(name = nil, breed = nil, age = nil)
+  @name = name
+  @breed = breed
+  @age = age
+  @@all << self
+end
 
-  def save
-   @@all << self
-  end
+def self.all
+  @@all
+end
 
-#keep track of each instance of dog created
-  def self.create(name, breed, age)
-    self.new(name, breed, age).save
-  end
 end
