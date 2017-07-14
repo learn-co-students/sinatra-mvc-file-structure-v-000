211efa149507ac5c9d8fa866beb2301855e9f9f1
emhopkins <epoletto@lstreetc.com>
2017-07-13 23:20:21 -0500

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,22 @@
-class Model
+class Dog 
+	attr_accessor :name, :age
+
+	@@all = []
+
+	def initialize(name, breed, age)
+		@name = name
+		@breed = breed
+		@age = age
+		@@all << self
+	end
+
+	def self.all 
+		@@all
+	end
+
+	def breed
+		@breed
+	end
+
 
 end
