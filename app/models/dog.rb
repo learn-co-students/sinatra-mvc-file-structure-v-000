494ee827e8c53958ac0c60fd88e7e50b59f29a8e
Pat  Pignataro <pat6284@msn.com>
2017-10-07 00:06:35 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  attr_accessor :name, :age, :breed 
+
+  @@dogs = []
+
+  def initialize(name, breed, age)
+
+    @name = name
+    @breed = breed
+    @age = age
+    @@dogs << self
+  end
+
+  def self.all
+    @@dogs 
+  end
+end    
