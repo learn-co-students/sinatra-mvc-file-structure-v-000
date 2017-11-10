732418a30c5b457aea7bad93f670ae2626bd3166
miriam <miriam5000@gmail.com>
2017-11-10 15:51:17 -0500

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  attr_accessor :name, :breed, :age
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @age = age
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def save 
+    self.all << self
+  end
+
+end 
