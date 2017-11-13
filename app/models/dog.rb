ae7f51c333f52f7fbbc1802340853a6dff45f289
Julianne Kim <juliannne@gmail.com>
2017-11-13 16:36:59 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,16 +2,16 @@ class Dog
   attr_accessor :name, :age
   attr_reader :breed
 
-  @@all = []
+  DOGS = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@all << self
+    DOGS << self
   end
 
   def self.all
-    @@all
-  end 
+    DOGS
+  end
 end
