e3cb3f9634b3d7cba1666acf0f10e8eafc9ff74e
nptravis <nptravis@yahoo.com>
2018-02-22 13:12:27 +0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
+  configure do 
+  	# tells controller where to find views and public dir
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
   get "/" do
+  	# loads the index.erb file
   	erb :index
   end
 end
