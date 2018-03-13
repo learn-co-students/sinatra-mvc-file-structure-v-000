e384c7955b0724ff7337fac25f3483468594b6e6
thesilentc <thesilentc@me.com>
2018-03-12 23:45:32 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
+class Dog
+  attr_accessor :name, :age
+  attr_reader :breed
+  @@all = []
 
+  def initialize(name, breed, age)
+    @name = name
+    @age = age
+    @breed = breed
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
