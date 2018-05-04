44436e95ad847f5176fda6a5ef4741a87633de0e
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-04 20:41:03 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,17 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    @@all << self
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  def self.all
+    @@all
+  end
 
 end
