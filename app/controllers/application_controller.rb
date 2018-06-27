8518c21cfc5a405fd1730ef3216adf9b51eb4fcf
Matt Farmer <mrfarmer777@gmail.com>
2018-06-27 04:30:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+  	set :views, "app/views"   #tells the controller where the views are, I guess?
+  	set :public_dir, "public" #tells the controller where public asset files are??
   end
 
   get "/" do
