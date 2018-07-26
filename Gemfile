0d30d0e2f6c764f36217ab4c61e974f36260fddd
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-26 16:28:10 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rake'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
+gem 'activerecord', '~> 4.2'
 
 group :test do
   gem 'rspec'
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ ENV['RACK_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
+require 'active_record'
+require 'rake'
 require_all 'app'
