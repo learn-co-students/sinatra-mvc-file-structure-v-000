e418d10d628d0adfc8da5a130f395ff96061f310
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-08 18:12:18 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
+  @@dogs = []
 
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@dogs << self
+  end
+
+  def self.all
+    @@dogs
+  end
 end
