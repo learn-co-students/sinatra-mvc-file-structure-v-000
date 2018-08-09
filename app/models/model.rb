7a8b3f0d31cd5ddb8144b3d7fcde1053b920354f
ValerieHMoore <periwinkle17@outlook.com>
2018-08-09 12:11:09 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,21 @@
-class Model
+class Dog
+    attr_accessor :name, :breed, :age
 
+    @@all = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all << self
+    end
+    
 end
