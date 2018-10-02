d56573ed2a6e6361efc8cec4e2e8de96eaff41f8
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-02 09:52:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
+
 class ApplicationController < Sinatra::Base
 
   configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+    set :views, "app/views"
+    set :public_dir, "public"
   end
 
-  get "/" do
-  	erb :index
+  get '/' do
+    erb :index
   end
 end
