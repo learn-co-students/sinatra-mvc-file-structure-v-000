1bc58c10f99d0e302625670a90649d79e8ae3b89
Harley Rayner <harleymteden@gmail.com>
2018-11-21 01:04:20 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,15 @@
-class Dog < Sinatra::Base
+class Dog
+
   attr_accessor :name, :breed, :age
+
+  @@all = []
 
   def self.all
     @@all
   end
 
   def initialize(name, breed, age)
+    # binding.pry
     @name = name
     @breed = breed
     @age = age
