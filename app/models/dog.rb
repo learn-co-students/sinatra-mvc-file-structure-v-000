2e51dddf1f4ea9f0ad71a286e27f69558a2543c8
Stef Claus <stef.claus@gmail.com>
2019-03-03 15:15:33 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+
+  
+end 
+
+
+# his class should have name, breed, and age attributes which can be set on initialization. 
+#You should be able to read and write to these attributes. 
+#This class should also keep track of each instance of dog created, 
+#as well as a class method all to return an array of those instances.
