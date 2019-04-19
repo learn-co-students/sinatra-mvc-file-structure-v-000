99dbd12d5accbb373917d36c4238d029e59f25ae
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-19 13:47:09 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
   attr_accessor :name, :breed, :age
-
+  
   @@all = []
 
   def initialize(name, breed, age)
@@ -9,6 +9,16 @@ class Dog
     @breed = breed
     @age = age
     @@all << self
+  end
+  
+#   get '/all' do
+#   # @dog = Dog.all
+ 
+#   # erb :'all/index.html.erb'
+# end
+  
+  def all
+    @@all
   end
 
 end
