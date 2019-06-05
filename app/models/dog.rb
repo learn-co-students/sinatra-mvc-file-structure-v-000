9f66962d716b2dd6776786e44a926320f19d5454
Amee Covarrubias <ameecova@yahoo.com>
2019-06-05 01:13:15 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Dog
+  attr_accessor :name, :breed, :age
+  
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    self.class.all << self
+  end
+  
+  def self.all 
+    @@all
+  end 
+end 
