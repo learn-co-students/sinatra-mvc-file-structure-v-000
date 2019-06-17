c1087bf5e4e0f9cb340879ff8e1e61824a98f7b1
Steven Crouse <sdcrouse91@gmail.com>
2019-06-17 00:08:52 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,9 +4,11 @@ class Dog
   @@all = []
   
   def initialize(name = "Snoopy", breed = "Beagle", age = 70)
-    @name = name 
-    @breed = breed 
-    @age = age
+    #@name = name 
+    #@breed = breed 
+    #@age = age
+    
+    @name, @breed, @age = name, breed, age # Interesting; this sets the values AND returns an array of those values!
     
     @@all << self
   end
