eeccd0099310b1c75337316e7479992ee3146021
Sophie Gu <sophieqgu@gmail.com>
2019-08-15 02:25:32 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
+class Dog 
+  attr_accessor :name, :breed, :age 
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @age = age 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 
 end
