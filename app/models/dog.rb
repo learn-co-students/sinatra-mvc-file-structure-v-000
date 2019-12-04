53e321470eeff664e952e1d28c6c5d1add66342c
Niusha H <niusha.hadziomerovic@gmail.com>
2019-12-04 01:14:56 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+  attr_accessor :name, :breed, :age 
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+  
+end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,3 @@
 class Model
-
+  Dog.new(Lola, prince_charles, 12)
 end
